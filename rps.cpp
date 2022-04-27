8de9bafc2a97619d46772613dba7fb9e61beaf88
losowy <arandomone.businnes@gmail.com>
2022-04-27 19:27:08 +0200

    yes

--- rps.cpp
+++ rps.cpp
@@ -1,40 +1,54 @@
 #include <iostream>
 #include <vector>
 #include <time.h>
+#include <string>
 
-int main() {
-	srand(time(0));
-	
+int main() {	
+	int iScore;
+	std::string sPlayAgain;
 	std::string sUserIn;
-	std::vector<std::string> vChoices = {"rock", "paper", "scissors"};
-	std::string sBot = vChoices[rand() % 3 + 1];
+	std::vector<std::string> vChoices;
 
-	std::cout << "Rock, paper, scissors, shoot! ";
-	std::cin >> sUserIn;
+	sPlayAgain = "y";
+	iScore = 0;
+	vChoices = {"rock", "paper", "scissors"};
 
-	if (sBot == sUserIn) {
-		std::cout << "It's a tie!\n";
-	} else if (sBot == "rock") {
-		if (sUserIn == "paper") {
-			std::cout << "You win!\n";
-		} else if (sUserIn == "scissors") {
-			std::cout << "You lose!\n";
+	while (sPlayAgain.rfind("y", 0) || sPlayAgain.rfind("Y", 0)) {
+		srand(time(0));
+		std::string sBot = vChoices[rand() % 3 + 1];
+
+		std::cout << "Rock, paper, scissors, shoot! ";
+		std::cin >> sUserIn;
+
+		if (sBot == sUserIn) {
+			std::cout << "It's a tie!\n";
+		} else if (sBot == "rock") {
+			if (sUserIn == "paper") {
+				std::cout << "You win!\n";
+			} else if (sUserIn == "scissors") {
+				std::cout << "You lose!\n";
+			}
+		} else if (sBot == "paper") {
+			if (sUserIn == "rock") {
+				std::cout << "You lose!\n";
+			} else if (sUserIn == "scissors") {
+				std::cout << "You win!\n";
+			}
+		} else if (sBot == "scissors") {
+			if (sUserIn == "rock") {
+				std::cout << "You win!\n";
+			} else if (sUserIn == "paper") {
+				std::cout << "You lose!\n";
+			}
 		}
-	} else if (sBot == "paper") {
-		if (sUserIn == "rock") {
-			std::cout << "You lose!\n";
-		} else if (sUserIn == "scissors") {
-			std::cout << "You win!\n";
-		}
-	} else if (sBot == "scissors") {
-		if (sUserIn == "rock") {
-			std::cout << "You win!\n";
-		} else if (sUserIn == "paper") {
-			std::cout << "You lose!\n";
-		}
+
+		iScore++;
+
+		std::cout << "Play again? (y/n)\n";
+		std::cin >> sPlayAgain;
 	}
 
-	std::cout << "Thank's for playing!\n\n - losowy\n";
+	std::cout << "Thank's for playing!\nYou won " << iScore << " times.\n\n - losowy\n";
 	std::cin.get();
 
 	return 0;
